6a4eb5e5db6a19c302296877dc2dc41038dec114
50488 <50488@192.168.0.19>
2025-01-31 10:46:36 -0600

    Agregué autenticación de usuario

---
+++ pruebagit/src/pruebagit/clase_pruebagit.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package pruebagit;
+
+/**
+ *
+ * @author 50488
+ */
+public class clase_pruebagit {
+    
+    public void imprimir(){
+        System.out.println("MAMIA PISA BURRAS ");
+    }
+}
